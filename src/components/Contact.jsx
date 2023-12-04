91197cd1dd971b23b914215520d26dc568ce8377
jaedenHob <jaedenhuxtable@gmail.com>
2023-12-04 16:59:02 -0500

    added a loading feature so users can see that the site is still processing form submission.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,10 +9,14 @@ function Contact() {
         message: '',
     });
 
+    const [loading, setLoading] = useState(false);
+
     const handleFormSubmit = async (e) => {
         e.preventDefault();
 
         try {
+            setLoading(true);
+
             const response = await fetch('https://contacts-requests.onrender.com/contact', {
                 method: 'POST',
                 headers: {
@@ -23,17 +27,27 @@ function Contact() {
 
             if (response.ok) {
                 // Handle success
+
+                setFormData({
+                    name: '',
+                    phone: '',
+                    email: '',
+                    subject: '',
+                    message: '',
+                });
+
                 alert('form submission is successful');
+
             } else {
                 // Handle error
                 alert('form submission was unsuccessful');
             }
-
-            // window.location.reload();
         
         } catch (error) {
             // Handle network error
             console.error('Error: ', error);
+        } finally {
+            setLoading(false);
         }
 
     };
@@ -42,15 +56,37 @@ function Contact() {
         const { name, value } = e.target;
         setFormData({ ...formData, [name]: value });
     };
+    
 
     return (
-        <div id='contact' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
+        <div id='contact' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16 relative'>
+            {loading ? (
+                    <div className=' w-full h-full bg-slate-50 bg-opacity-70 rounded-lg left-8 absolute flex items-center justify-center'>
+
+                        <div>
+                            <p className='text-3xl font-bold flex items-center justify-center'>Sumbitming form may take 1-3 minutes</p>
+                            <p className='text-3xl font-bold flex items-center justify-center p-5'>please do not close browser...</p>
+
+                            <div class="flex space-x-5 items-center justify-center">
+                                <span class="circle [animation-delay:-0.1s] animate-bounce-color-change"></span>
+                                <span class="circle [animation-delay:-0.2s] animate-bounce-color-change"></span>
+                                <span class="circle [animation-delay:-0.3s] animate-bounce-color-change"></span>
+                                <span class="circle [animation-delay:-0.4s] animate-bounce-color-change"></span>
+                            </div>
+
+                        </div>
+                    </div>) : (
+                    <div>
+
+                    </div>
+                )}
+                    
             <h1 className='py-4 text-4xl font-bold text-center text-black'>Contact Me</h1>
 
             <form onSubmit={handleFormSubmit}>
-                
+
                 <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
-                
+
                     <div className='flex flex-col'>
                         <label className='uppercase text-sm py-2'>Name</label>
                         <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" name="name" value={formData.name} onChange={handleInputChange} />
@@ -74,20 +110,19 @@ function Contact() {
                     <textarea
                         maxLength={600}
                         placeholder='message (max 600 characters)'
-                        className='border-2 rounded-lg p-3 border-gray-300' 
-                        rows='10' 
-                        type='text' 
-                        name='message' 
+                        className='border-2 rounded-lg p-3 border-gray-300'
+                        rows='10'
+                        type='text'
+                        name='message'
                         value={formData.message}
-                        onChange={handleInputChange}/>
+                        onChange={handleInputChange} />
                 </div>
 
-                <button type="submit" className="bg-yellow-400 text-black mt-4 w-full p-4 rounded-lg">
-                    Submit
+                <button type="submit" className="bg-yellow-400 text-black mt-4 w-full p-4 rounded-lg" disabled={loading}>
+                    {loading ? 'Submitting...' : 'Submit'}
                 </button>
             </form>
         </div>
-        
     );
 }
 
